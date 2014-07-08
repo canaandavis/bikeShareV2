c8304e5602624a6d50a1452ff4af1e1f8e8e50b2
Canaan Davis <canaandavis@Canaans-MacBook-Pro.local>
2014-07-08 07:07:41 -0500

    Add app view

--- app/js/views/app.js
+++ app/js/views/app.js
@@ -1,1 +1,62 @@
-app.js
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'js/collections/stations',
+  'js/views/station',
+  'js/views/map'
+  ], function($, _, Backbone, Stations, StationView, MapView){
+    var AppView = Backbone.View.extend({
+
+      el: $('.app-wrapper'),
+
+      events: {
+        'click .filter' : 'addFilter'
+      },
+
+      initialize: function(){
+        this.that = this;
+        this.collection = new Stations();
+        this.collection.bind('reset', this.buildMap, this);
+        this.collection.bind('reset', this.addAllStations, this);
+        this.collection.fetch({ reset: true });
+      },
+
+      addOne: function(station) {
+        var view = new StationView({ model: station });
+        $('#stations').append(view.render().el);
+      },
+
+      addAllStations: function(){
+        this.collection.each(this.addOne);
+      },
+
+      addFilter: function(filter) {
+        var filter = $(filter.target).val();
+        var stations = this.collection.filterLocations(filter);
+        this.updateLocationList(stations);
+        this.updateMap(stations);
+      },
+
+      buildMap: function(collection) {
+        this.map = new MapView({ collection : collection.models });
+        console.log(this.map);
+        $('#map').append(this.map.render().el);
+      },
+
+      updateLocationList: function(stations) {
+        var that = this;
+        this.$('#stations').html('');
+        _.each(stations, function(station){
+          that.addOne(station);
+        });
+      },
+
+      updateMap: function(stations) {
+        this.$('#map').html('');
+        this.map.updateMap(stations);
+      },
+
+    });
+    return AppView;
+  });
