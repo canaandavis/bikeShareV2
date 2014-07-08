aed1ef91d6a7a5317f7ab9c0bca6f5e14bb06b4a
Canaan Davis <canaandavis@Canaans-MacBook-Pro.local>
2014-07-08 07:03:18 -0500

    Initial file set up

---
+++ app/main.js
@@ -0,0 +1,12 @@
+require.config({
+  paths: {
+    jquery: 'bower_components/jquery/dist/jquery',
+    underscore: 'bower_components/underscore/underscore',
+    backbone: 'bower_components/backbone/backbone',
+    text: 'js/libs/require/text'
+  }
+});
+
+require(['js/views/app'], function(AppView){
+  var app_view = new AppView;
+});
