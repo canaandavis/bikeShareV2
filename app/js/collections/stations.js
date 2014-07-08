5c2385011304f7e0a6ba851d3351206723a1ca1a
Canaan Davis <canaandavis@Canaans-MacBook-Pro.local>
2014-07-08 07:06:19 -0500

    Add Stations Collection

--- app/js/collections/stations.js
+++ app/js/collections/stations.js
@@ -1,1 +1,24 @@
-stations.js
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'js/models/station'
+  ], function($, _, Backbone, Station){
+  var Stations = Backbone.Collection.extend({
+    url: "http://api.citybik.es/v2/networks/austin?fields=stations",
+
+    model: Station,
+
+    parse: function(response) {
+      return response.network.stations;
+    },
+
+    filterLocations: function(filter){
+      return _.filter(this.models, function(station){
+        return station.isInArea(filter);
+      });
+    }
+
+  });
+  return Stations;
+});
