31535620120e9ecc3bd13a8248cce0bf99abf80e
Canaan Davis <canaandavis@Canaans-MacBook-Pro.local>
2014-07-09 17:42:57 -0500

    Update dependencies

--- app/main.js
+++ app/main.js
@@ -7,10 +7,17 @@ require.config({
     foundation: 'bower_components/foundation/js/foundation',
     offCanvas: 'bower_components/foundation/js/foundation/foundation.offcanvas',
     text: 'js/libs/require/text'
+  },
+  shim: {
+    'foundation': {
+      deps: ['jquery'],
+      exports: 'foundation'
+    }
   }
 });
 
-require(['js/views/app'], function(AppView){
+require(['js/views/app', 'foundation', 'offCanvas'], function(AppView){
+  console.log($);
   var app_view = new AppView;
   $(document).foundation();
   setInterval(function(){
