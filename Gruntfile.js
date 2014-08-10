f3068e0e9f164fae05467ab6f93a57a4f5169e3c
Canaan Davis <canaandavis@gmail.com>
2014-08-09 19:51:23 -0500

    Update build

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,39 +5,5 @@
  * Copyright (c) 2012 Sindre Sorhus, contributors
  * Licensed under the MIT license.
  */
-(function () {
-   'use strict';
-}());
 
-module.exports = function (grunt) {
-grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    sass: {
-      dist: {
-        files: {
-          'app/css/style.css' : 'app/sass/style.scss'
-        }
-      }
-    },
-    jshint: {
-      all: {
-        src: ['Gruntfile.js', 'app/js/**/*.js', 'app/main.js']
-      }
-    },
-
-    watch: {
-      css: {
-        files: '**/*.scss',
-        tasks: ['sass']
-      },
-      jshint: {
-        files: ['Gruntfile.js', 'app/js/**/*.js', 'app/main.js'],
-        task: ['jshint']
-      }
-    }
-  });
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.registerTask('default',['watch', 'jshint']);
-};
+(function(){})(),module.exports=function(e){e.initConfig({pkg:e.file.readJSON("package.json"),sass:{dist:{files:{"app/css/style.css":"app/sass/style.scss"}}},jshint:{all:{src:["Gruntfile.js","app/js/**/*.js","app/main.js"]}},watch:{css:{files:"**/*.scss",tasks:["sass"]},jshint:{files:["Gruntfile.js","app/js/**/*.js","app/main.js"],task:["jshint"]}}}),e.loadNpmTasks("grunt-contrib-sass"),e.loadNpmTasks("grunt-contrib-watch"),e.loadNpmTasks("grunt-contrib-jshint"),e.registerTask("default",["watch","jshint"])};
